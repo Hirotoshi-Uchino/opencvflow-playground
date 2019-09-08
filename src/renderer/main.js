ba0cc3a554908919d1206c212ce8edb09ccbc1ad
hirotoshi <u.hirotoshi@gmail.com>
2019-09-08 20:42:54 +0900

    add vuetify

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,4 +1,6 @@
 import Vue from 'vue'
+import Vuetify from 'vuetify';
+import "vuetify/dist/vuetify.min.css";
 import axios from 'axios'
 
 import App from './App'
@@ -6,8 +8,11 @@ import router from './router'
 import store from './store'
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
+Vue.use(Vuetify);
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
+
+
 
 /* eslint-disable no-new */
 new Vue({

eacef3c3107954a3e6c7b3bf4cea66149acd0c33
hirotoshi <u.hirotoshi@gmail.com>
2019-09-08 21:29:38 +0900

    [update] add link to home

--- src/renderer/components/DragDropPage.vue
+++ src/renderer/components/DragDropPage.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>Vue-konva のサンプルをコピペ。v-layer を使うために webpack.renderer.config.js を修正する必要があることに注意。(<a href="https://github.com/konvajs/vue-konva/issues/14">https://github.com/konvajs/vue-konva/issues/14</a>)
+  <div>
+    <router-link to="/">ホームへ</router-link><br>
+    Vue-konva のサンプルをコピペ。v-layer を使うために webpack.renderer.config.js を修正する必要があることに注意。(<a href="https://github.com/konvajs/vue-konva/issues/14">https://github.com/konvajs/vue-konva/issues/14</a>)
     <v-stage
       ref="stage"
       :config="configKonva"

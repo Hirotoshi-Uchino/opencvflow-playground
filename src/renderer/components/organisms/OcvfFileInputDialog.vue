74ae79b60a7d2af89c9e7863a983e644fd2abced
hirotoshi <u.hirotoshi@gmail.com>
2019-10-14 14:26:55 +0900

    [update] design of FileInputDialog

--- src/renderer/components/organisms/OcvfFileInputDialog.vue
+++ src/renderer/components/organisms/OcvfFileInputDialog.vue
@@ -1,26 +1,44 @@
 <template>
   <dialog>
     <form method="dialog">
-      <input type="file" id="select-file" @click="setFileEvent" accept="image/png,image/jpge,image/bmp">
-      <br>
+      <header class="toolbar toolbar-header">
+        <h1 class="title">画像ファイル入力</h1>
+      </header>
+
+      <div id="input-area">
+        <label for="select-file">
+          <span class="btn btn-default">ファイルを選択</span>
+          <span style="text-align: center">
+            {{imageFileName}}
+          </span>
+          <input
+              type="file"
+              id="select-file"
+              @click="setFileEvent"
+              accept="image/png,image/jpge,image/bmp"
+          >
+        </label>
+      </div>
       <img :src="imageData" alt="">
 
-      <menu>
-        <span class="btn btn-default">リセット</span>
-        <button class="btn btn-primary active pull-right">完了</button>
-      </menu>
+      <footer class="toolbar toolbar-footer">
+        <span class="btn btn-default reset-btn">リセット</span>
+        <button class="btn btn-primary pull-right complete-btn">完了</button>
+      </footer>
     </form>
   </dialog>
-
 </template>
 
 <script>
+  // TODO: input parameter to block
   export default {
     name: "OcvfFileInputDialog",
 
     data: function(){
       return {
-        imageData: ""
+        imageFileName: '',
+        imageFilePath: '',
+        imageData: ''
       }
     },
 
@@ -30,41 +48,62 @@
 
         let objFile = document.getElementById("select-file");
         objFile.addEventListener("change", function(evt) {
-          let file = evt.target.files[0];
-          let reader = new FileReader();
-          reader.readAsDataURL(file);
-          reader.onload = function(e) {
-            _vm.imageData = e.target.result
-          };
+          console.log(evt.target)
+          try{
+            let file = evt.target.files[0];
+            let reader = new FileReader();
+
+            _vm.imageFileName = file.name
+            _vm.imageFilePath = file.path
+            reader.readAsDataURL(file);
+            reader.onload = function(e) {
+              _vm.imageData = e.target.result
+            };
+          } catch (e) {
+            console.log('input File Error')
+            console.log(e)
+          }
         }, false)
       }
-    }
+    },
+
   }
 </script>
 
 <style scoped>
-  /*#input-image{*/
-    /*vertical-align: middle;*/
-    /*display: block;*/
-  /*}*/
-
   img{
     margin: auto;
     margin-top: 10px;
     margin-bottom: 10px;
     width: 350px;
-    /*border: solid 1px black;*/
     display: block;
-    /*max-width: 100%;*/
   }
 
+  #select-file{
+    display:none
+  }
+
+  #input-area{
+    margin: 10px;
+  }
 
   dialog {
     width: 400px;
     /*height: 100%;*/
     position: absolute;
-    top: 0;
+    top: 30px;
     left: 50%;
     transform: translate(-50%, 0);
+    border: solid 1px #aaa;
+    border-radius: 5px;
+    box-shadow: 0 8px 10px 1px rgba(0,0,0,0.14), 0 3px 14px 2px rgba(0,0,0,0.12), 0 5px 5px -3px rgba(0,0,0,0.3);
+  }
+
+  .reset-btn{
+    margin: 5px;
+  }
+
+  .complete-btn{
+    margin: 5px;
   }
 </style>

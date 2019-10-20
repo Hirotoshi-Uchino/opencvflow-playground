72bc14efad6fd18a36bf4ff53dea942573eea980
hirotoshi <u.hirotoshi@gmail.com>
2019-10-20 14:05:34 +0900

    [delete] venv

---
+++ src/renderer/components/Konva.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-stage
+      id="canvas"
+      ref="stage"
+      :config="configKonva"
+  >
+    <v-layer ref="layer">
+      <CustomBlock
+          :text-label="text"
+      />
+    </v-layer>
+  </v-stage>
+</template>
+
+<script>
+
+  import Vue from 'vue';
+  import VueKonva from 'vue-konva';
+  import CustomBlock from './atoms/CustomBlock'
+
+  Vue.use(VueKonva);
+
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+
+  export default {
+    name: "Konva",
+    components: {CustomBlock},
+    data() {
+      return {
+        configKonva: {
+          width: width,
+          height: height
+        },
+        text: "TEST"
+      };
+    },
+  };
+
+</script>
+
+<style scoped>
+  #canvas {
+    width: 800px;
+    height: 800px;
+  }
+</style>
